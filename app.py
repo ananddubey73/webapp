b8fa982e5ce62bbc5a8b8b8097d20b5d8ebb44ac
ananddubey73 <dubeyanand767744@gmail.com>
2024-04-16 13:25:45 +0530

    change time and format

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
 from flask import Flask, render_template
 import requests
+from datetime import datetime
 
-app = Flask(__name__)
+app = Flask(_name_)
 
 def get_random_quote():
     response = requests.get("https://api.quotable.io/random")
     if response.status_code == 200:
         data = response.json()
-        return f"{data['content']} - {data['author']}"
+        return data['content'], data['author']
     else:
-        return "Failed to fetch the quote."
+        return "Failed to fetch the quote.", ""
 
 @app.route('/')
 def index():
-    quote = get_random_quote()
-    return render_template('index.html', quote=quote)
+    quote, author = get_random_quote()
+    current_time = datetime.now().strftime('%H:%M:%S')
+    return render_template('index.html', quote=quote, author=author, current_time=current_time)
 
-if __name__ == '__main__':
+if _name_ == '_main_':
     app.run(host='0.0.0.0', port=80)
